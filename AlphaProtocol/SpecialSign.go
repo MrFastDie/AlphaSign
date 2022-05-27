527560171f6d7c8f745d203f35ac3a4b58a5be68
MrFastDie <dhackbarth@extr3m.com>
2022-05-27 06:54:58 +0200

    special sign func

--- AlphaProtocol/SpecialSign.go
+++ AlphaProtocol/SpecialSign.go
@@ -3,15 +3,30 @@ package AlphaProtocol
 type SpecialSign byte
 
 const (
-	SPEZIAL_SIGN_THANK_YOU            SpecialSign = 0x53
-	SPEZIAL_SIGN_NO_SMOKE             SpecialSign = 0x55
-	SPEZIAL_SIGN_DONT_DRINK_AND_DRIVE SpecialSign = 0x56
-	SPEZIAL_SIGN_RUNNING_ANIMAIL      SpecialSign = 0x57
-	SPEZIAL_SIGN_FIREWORKS            SpecialSign = 0x58
-	SPEZIAL_SIGN_TURBO_CAR            SpecialSign = 0x59
-	SPEZIAL_SIGN_CHERRY_BOMB          SpecialSign = 0x5A
+	SPECIAL_SIGN_THANK_YOU            SpecialSign = 0x53
+	SPECIAL_SIGN_NO_SMOKE             SpecialSign = 0x55
+	SPECIAL_SIGN_DONT_DRINK_AND_DRIVE SpecialSign = 0x56
+	SPECIAL_SIGN_RUNNING_ANIMAIL      SpecialSign = 0x57
+	SPECIAL_SIGN_FIREWORKS            SpecialSign = 0x58
+	SPECIAL_SIGN_TURBO_CAR            SpecialSign = 0x59
+	SPECIAL_SIGN_CHERRY_BOMB          SpecialSign = 0x5A
 )
 
-func CreateSpecialSign(position Position, specialSign SpecialSign) []byte {
+func createSpecialSign(position Position, specialSign SpecialSign) []byte {
 	return []byte{0x41, 0x41, 0x1B, byte(position), 0x6E, byte(specialSign)}
+}
+
+func SetSpecialSign(position Position, specialSign SpecialSign) []byte {
+	sequences := [][]byte{
+		InitSequence(),
+		createSpecialSign(position, specialSign),
+		DeinitSequence(),
+	}
+
+	var returnSequence []byte
+	for _, sequence := range sequences {
+		returnSequence = append(returnSequence, sequence...)
+	}
+
+	return returnSequence
 }
--- main.go
+++ main.go
@@ -19,7 +19,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, err = s.Write(AlphaProtocol.SetStaticText(append([]byte("Hey, its: "), AlphaProtocol.PrepareDateTime()...), AlphaProtocol.COLOR_GREEN))
+	//_, err = s.Write(AlphaProtocol.SetStaticText(append([]byte("Hey, its: "), AlphaProtocol.PrepareDateTime()...), AlphaProtocol.COLOR_COLOR_MIX))
+	//_, err = s.Write(AlphaProtocol.SetSpecialText([]byte("Sex?"), AlphaProtocol.POSITION_MIDDLE, AlphaProtocol.DEFAULT_MODE_FLASH, AlphaProtocol.SPECIAL_MODE_SNOW))
+	_, err = s.Write(AlphaProtocol.SetStaticText([]byte("Hey Jonas!"), AlphaProtocol.COLOR_RED))
+	//_, err = s.Write(AlphaProtocol.SetSpecialSign(AlphaProtocol.POSITION_MIDDLE, AlphaProtocol.SPECIAL_SIGN_FIREWORKS))
 	if err != nil {
 		log.Fatal(err)
 	}
